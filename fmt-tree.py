d5352fc449b4f525d2ca8249a0f010acc4054c68
Ramūnas Mažeikis <ramunas.mazeikis@teltonika.lt>
2024-05-20 15:06:47 +0300

    Slight refactor and an extra option.
    Signed-off-by: Ramūnas Mažeikis <ramunas.mazeikis@teltonika.lt>

--- fmt-tree.py
+++ fmt-tree.py
@@ -20,6 +20,7 @@ def main():
     parser.add_argument('--item', '--item-marker', type=str, default='__ ')
     parser.add_argument('--last-item', '--last-item-marker', type=str, default=r'\_ ')
     parser.add_argument('--os', '--output-separator', type=str, default=' ')
+    parser.add_argument('--full-text', action='store_true')
 
     output_width = 120
 
@@ -48,9 +49,21 @@ def main():
     else:
         field_extractor = FieldSplitter(r'\s+')
 
+    if args.full_text:
+        node_formatter = FullTextNodeFormatter()
+    else:
+        node_formatter = NodeFieldFormatter(args.os, exclude=[args.id, args.parent])
+
+    tree_formatter = AsciiTreeFormatter(
+            args.output_width,
+            node_formatter,
+            indent_marker=args.indent,
+            item_marker=args.item,
+            last_item_marker=args.last_item)
+
     nodes = {}
 
-    for line in sys.stdin:
+    for line in filter(lambda s: len(s) > 0, sys.stdin):
         line = line.strip()
         node = Node(line, field_extractor.get_fields(line))
         nodes[node.fields[args.id]] = node
@@ -77,76 +90,90 @@ def main():
         node.subnodes.sort(key=get_id)
 
     for node in roots:
-        print(formatNode(
-            node,
-            args.output_width,
-            field_separator=args.os,
-            exclude=[args.id, args.parent],
-            indent_marker=args.indent,
-            item_marker=args.item,
-            last_item_marker=args.last_item))
+        print(tree_formatter.format_tree(node))
 
-def formatNode(
-        node,
-        output_width,
-        field_separator=' ',
-        exclude=[],
-        indent_marker='  |',
-        item_marker='__ ',
-        last_item_marker='\\_'):
-    class PrintState:
-        def __init__(self, node):
-            self.node = node
-            self.at = 0
-            self.printed = False
+class FullTextNodeFormatter:
+    def __init__(self):
+        pass
 
-    print_states = [ PrintState(node) ]
-    at = 0
+    def format_node(self, node):
+        return node.full_line
 
-    last_indent_marker = ' ' * len(indent_marker)
-    lines = []
+class NodeFieldFormatter:
+    def __init__(self, field_separator=' ', exclude=[]):
+        self.exclude = exclude
+        self.field_separator = field_separator
 
-    while len(print_states) > 0:
-        state = print_states[at]
+    def format_node(self, node):
+        data_tokens = []
 
-        if not state.printed:
-            tokens = []
+        for i in range(len(node.fields)):
+            if i not in self.exclude:
+                data_tokens.append(node.fields[i])
 
-            for prev_state in print_states[:-1]:
-                if prev_state.at < len(prev_state.node.subnodes):
-                    tokens.append(indent_marker)
-                else:
-                    tokens.append(last_indent_marker)
+        return self.field_separator.join(data_tokens)
 
-            if at > 0:
-                prev_state = print_states[at - 1]
+class AsciiTreeFormatter:
+    def __init__(self,
+            output_width,
+            node_formatter,
+            indent_marker='  |',
+            item_marker='__ ',
+            last_item_marker='\\_'):
 
-                if prev_state.at < len(prev_state.node.subnodes):
-                    tokens.append(item_marker)
-                else:
-                    tokens.append(last_item_marker)
+        self.output_width = output_width
+        self.node_formatter = node_formatter
+        self.indent_marker = indent_marker
+        self.item_marker = item_marker
+        self.last_item_marker = last_item_marker
 
-            data_tokens = []
+    def format_tree(self, node):
+        class PrintState:
+            def __init__(self, node):
+                self.node = node
+                self.at = 0
 
-            for i in range(len(state.node.fields)):
-                if i not in exclude:
-                    data_tokens.append(state.node.fields[i])
+        print_states = [ PrintState(node) ]
+        at = 0
 
-            tokens.append(field_separator.join(data_tokens))
-            lines.append(''.join(tokens)[0:output_width])
-            state.printed = True
+        last_indent_marker = ' ' * len(self.indent_marker)
+        lines = []
 
-        if state.at < len(state.node.subnodes):
-            node = state.node
-            print_states.append(PrintState(node.subnodes[state.at]))
-            state.at += 1
-            at += 1
-        else:
-            print_states.pop()
-            at -= 1
-            state.at += 1
+        while len(print_states) > 0:
+            state = print_states[at]
 
-    return '\n'.join(lines)
+            if state.at == 0:
+                tokens = []
+
+                for prev_state in print_states[:-1]:
+                    if prev_state.at < len(prev_state.node.subnodes):
+                        tokens.append(self.indent_marker)
+                    else:
+                        tokens.append(last_indent_marker)
+
+                if at > 0:
+                    prev_state = print_states[at - 1]
+
+                    if prev_state.at < len(prev_state.node.subnodes):
+                        tokens.append(self.item_marker)
+                    else:
+                        tokens.append(self.last_item_marker)
+
+                tokens.append(self.node_formatter.format_node(state.node))
+                lines.append(''.join(tokens)[0:self.output_width])
+                state.printed = True
+
+            if state.at < len(state.node.subnodes):
+                node = state.node
+                print_states.append(PrintState(node.subnodes[state.at]))
+                state.at += 1
+                at += 1
+            else:
+                print_states.pop()
+                at -= 1
+                state.at += 1
+
+        return '\n'.join(lines)
 
 class FieldFinder:
     def __init__(self, field_pattern):
